ee6f16276d3f3fb3047d55505108f5cf396608b3
rofiadhimPS <muhammadrofiadhim@gmail.com>
2023-09-18 10:24:30 +0700

    add moodle pack

---
+++ theme/edumy/ccn/page_handler/ccn_page_handler.php
@@ -0,0 +1,48 @@
+<?php
+/*
+@ccnRef: @
+*/
+
+defined('MOODLE_INTERNAL') || die();
+include_once($CFG->dirroot . '/course/lib.php');
+include_once($CFG->dirroot . '/theme/edumy/ccn/mdl_handler/ccn_mdl_handler.php');
+
+class ccnPageHandler {
+
+  public function ccnGetPageTitle() {
+    global $PAGE, $COURSE, $DB, $CFG;
+
+    $ccnReturn = $PAGE->heading;
+
+    if(
+      $DB->record_exists('course', array('id' => $COURSE->id))
+      && $COURSE->format == 'site'
+      && $PAGE->cm
+      && $PAGE->cm->name !== NULL
+    ){
+      $ccnReturn = $PAGE->cm->name;
+    } elseif($PAGE->pagetype == 'blog-index') {
+      $ccnReturn = get_string("blog", "blog");
+    }
+
+    if($DB->record_exists('course', array('id' => $COURSE->id))){
+		$course_url = $CFG->wwwroot . '/course/view.php?id='. $COURSE->id;
+		$ccnReturn = '<a href="' . $course_url . '" class="text-light text-decoration-none">' . $ccnReturn . '</a>';
+	}
+
+    return $ccnReturn;
+  }
+
+  public function ccnGetPageUrl() {
+    global $PAGE;
+
+    $ccnMdlHandler = new ccnMdlHandler();
+    $ccnReturn = NULL;
+    if($PAGE){
+      $ccnPageUrlObj = $PAGE->url;
+      $ccnReflection = $ccnMdlHandler->ccnReflection($PAGE->url);
+      $ccnReturn = $ccnReflection;
+    }
+    return $ccnReturn;
+  }
+}
