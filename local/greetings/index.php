c729c88bfa21f8be0ef3517a834973bca517adc5
rofiadhimPS <muhammadrofiadhim@gmail.com>
2023-09-25 09:22:05 +0700

    Save and display greeting messages

--- local/greetings/index.php
+++ local/greetings/index.php
@@ -43,12 +43,33 @@
  }
 
  $messageform->display();
+ $messages = $DB->get_records('local_greetings_messages');  
 
  if ($data = $messageform->get_data()) {
-   $message = required_param('message', PARAM_TEXT);
+  $message = required_param('message', PARAM_TEXT);
 
-   echo $OUTPUT->heading($message, 4);
- }
+  if (!empty($message)) {
+      $record = new stdClass;
+      $record->message = $message;
+      $record->timecreated = time();
+
+      $DB->insert_record('local_greetings_messages', $record);
+  }
+}
+
+echo $OUTPUT->box_start('card-columns');
+
+foreach($messages as $msg) {
+  echo html_writer::start_tag('div', array('class' => 'card'));
+  echo html_writer::start_tag('div', array('class' => 'card-body'));
+  echo html_writer::tag('p', $msg->message, array('class' => 'card-text'));
+  echo html_writer::start_tag('p', array('class' => 'card-text'));
+  echo html_writer::tag('small', $msg->timecreated, array('class' => 'text-muted'));
+  echo html_writer::end_tag('p');
+  echo html_writer::end_tag('div');
+  echo html_writer::end_tag('div');
+}
+echo $OUTPUT->box_end();
 
  echo $OUTPUT->footer(); // display footer 
 
