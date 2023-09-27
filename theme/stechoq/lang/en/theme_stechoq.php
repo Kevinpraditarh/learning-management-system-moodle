92631a62a63b69294fb73f7445778f42c0e58d7c
rofiadhimPS <muhammadrofiadhim@gmail.com>
2023-09-27 14:03:56 +0700

    customize flat-navbar (sidebar)

--- theme/stechoq/lang/en/theme_stechoq.php
+++ theme/stechoq/lang/en/theme_stechoq.php
@@ -57,3 +57,4 @@ $string['rawscsspre'] = 'Raw initial SCSS';
 $string['rawscsspre_desc'] = 'In this field you can provide initialising SCSS code, it will be injected before everything else. Most of the time you will use this setting to define variables.';
 // We need to include a lang string for each block region.                                                                          
 $string['region-side-pre'] = 'Right';
+$string['fullname'] = 'Stechoq Academy';
